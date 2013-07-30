ef7af8513a432114c0c64811cad36e3f7d6221c6
Amey Kulkarni <amey.kulkarni@skhms.com>
2013-07-30 16:07:34 -0700

    Added basic navigation

---
+++ src/com/kulkarni/gasmileagecalculator/FillupFragment.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.kulkarni.gasmileagecalculator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Amey Kulkarni
+ *
+ */
+public class FillupFragment extends Fragment {
+
+	/**
+	 * 
+	 */
+	public FillupFragment() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fillup_fragment, container, false);
+		return rootView;
+	}
+
+}
--- src/com/kulkarni/gasmileagecalculator/MainActivity.java
+++ src/com/kulkarni/gasmileagecalculator/MainActivity.java
@@ -1,10 +1,15 @@
 package com.kulkarni.gasmileagecalculator;
 
+import java.util.Locale;
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,48 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    	
+    	public SectionsPagerAdapter (FragmentManager fm) {
+    		super (fm);
+    	}
+    	
+    	@Override
+    	public Fragment getItem (int position) {
+    		Fragment fragment;
+    		
+    		switch (position) {
+    		case 0:
+    			fragment = new FillupFragment ();
+    			break;
+    		case 1:
+    			fragment = new HistoryFragment ();
+    			break;
+    		default:
+    			fragment = null;
+    		}
+			
+    		return fragment;
+    	}
+    	
+    	@Override
+    	public int getCount () {
+    		// We have 2 pages currently
+    		return 2;
+    	}
+    	
+    	
+    	@Override
+    	public CharSequence getPageTitle (int position) {
+    		Locale l = Locale.getDefault();
+    		String[] page_titles = getResources().getStringArray(R.array.page_titles);
+    		
+    		return page_titles[position].toUpperCase(l);
+    	}
     }
     
 }
