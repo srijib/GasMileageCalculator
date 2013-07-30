f006f8288ee0094f51d4c742ec808a1948671475
Amey Kulkarni <amey.kulkarni@skhms.com>
2013-07-30 16:57:57 -0700

    Fixed default display fragment

--- src/com/kulkarni/gasmileagecalculator/FillupFragment.java
+++ src/com/kulkarni/gasmileagecalculator/FillupFragment.java
@@ -3,11 +3,16 @@
  */
 package com.kulkarni.gasmileagecalculator;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * @author Amey Kulkarni
@@ -25,7 +30,17 @@ public class FillupFragment extends Fragment {
 	@Override
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fillup_fragment, container, false);
+		
+		TextView dateTextView = (TextView) rootView.findViewById(R.id.textView_fillup_date);
+		dateTextView.setText(getTodaysDate());
 		return rootView;
 	}
 
+	
+	private CharSequence getTodaysDate () {
+		Date date = new Date ();
+		SimpleDateFormat sdf = (SimpleDateFormat) DateFormat.getDateInstance();
+		
+		return sdf.format(date);
+	}
 }
--- src/com/kulkarni/gasmileagecalculator/MainActivity.java
+++ src/com/kulkarni/gasmileagecalculator/MainActivity.java
@@ -7,14 +7,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
+	
+	SectionsPagerAdapter mSectionsPagerAdapter;
+	ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mSectionsPagerAdapter = new SectionsPagerAdapter (getSupportFragmentManager());
+        
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
     }
 
 
