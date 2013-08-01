5e22a191148972b25380357966b4c6c40427f81d
Amey Kulkarni <amey.kulkarni@skhms.com>
2013-07-31 18:57:49 -0700

    Added some date picker code

--- src/com/kulkarni/gasmileagecalculator/FillupFragment.java
+++ src/com/kulkarni/gasmileagecalculator/FillupFragment.java
@@ -5,9 +5,13 @@ package com.kulkarni.gasmileagecalculator;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +22,26 @@ import android.widget.TextView;
  * @author Amey Kulkarni
  *
  */
-public class FillupFragment extends Fragment {
+public class FillupFragment extends Fragment
+{
+	
+	public class DatePickerFragment extends DialogFragment implements
+			DatePickerDialog.OnDateSetListener {
+		
+		public Dialog onCreateDialog (Bundle savedInstanceState) {
+			Long curDate = savedInstanceState.getLong("setDate");
+			
+			final Calendar c = Calendar.getInstance();
+			c.setTimeInMillis(curDate);
+			int year  = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day   = c.get(Calendar.DAY_OF_MONTH);
+			
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+	}
+
+	public Date mDate;
 
 	/**
 	 * 
@@ -31,16 +54,27 @@ public class FillupFragment extends Fragment {
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fillup_fragment, container, false);
 		
+		mDate = new Date ();
+		
 		TextView dateTextView = (TextView) rootView.findViewById(R.id.textView_fillup_date);
-		dateTextView.setText(getTodaysDate());
+		dateTextView.setText(getDateString ());
 		return rootView;
 	}
 
 	
-	private CharSequence getTodaysDate () {
-		Date date = new Date ();
+	private CharSequence getDateString () {
 		SimpleDateFormat sdf = (SimpleDateFormat) DateFormat.getDateInstance();
-		
-		return sdf.format(date);
+		return sdf.format(mDate);
 	}
+	
+	public void onDateClicked (View v) {
+		Bundle currentDate = new Bundle ();
+		currentDate.putLong("setDate", mDate.getTime());
+		
+		DialogFragment newFragment = new DatePickerFragment ();
+		newFragment.setArguments(currentDate);
+		
+		newFragment.show(getFragmentManager(), "datePicker");
+	}
+	
 }
