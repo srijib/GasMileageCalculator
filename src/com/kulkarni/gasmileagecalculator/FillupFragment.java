f671e5a4ae6b2ac34223474280087ff41c7ff3df
Amey Kulkarni <amey.kulkarni@skhms.com>
2013-08-05 11:56:06 -0700

    Some cosmetic changes to the fillup fragment

--- src/com/kulkarni/gasmileagecalculator/FillupFragment.java
+++ src/com/kulkarni/gasmileagecalculator/FillupFragment.java
@@ -16,7 +16,6 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.TextView;
